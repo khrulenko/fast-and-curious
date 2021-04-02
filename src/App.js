8ce358ece7d47517db486bcf3e511d2347ab7717
Khrulenko Vitalii <khrulenko.v@gmail.com>
2021-04-03 01:43:46 +0300

    remade pagination style, added mobile adoptation

--- src/App.js
+++ src/App.js
@@ -66,6 +66,7 @@ export function App() {
         nextLinkClassName={"paginationNextLink"}
         previousLabel={"<"}
         nextLabel={">"}
+        disabledClassName={"paginationDisabled"}
       />}
     </>
   );
